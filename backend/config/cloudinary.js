56a8947e7ba3586a5f950aadf36393477c7fdba6
Dinakara Ambepitiya <dinakaranadun@gmail.com>
2025-11-14 19:26:51 +0530

    cloudinary fixes

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -3,19 +3,17 @@ import multer from 'multer'
 
 
 
-cloudinary.config({ 
-  cloud_name: 'ds76wzsb7', 
-  api_key: '238368551658762', 
-  api_secret: '0Fy7jAy80mldn0RPqb65rjhYkkk'
-});
-
-console.log("Cloudinary ENV:", process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
-
-
 const storage = new multer.memoryStorage();
 
 async function imageUploadUtil(file) 
 {
+      cloudinary.config({ 
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+      api_key: process.env.CLOUDINARY_API_KEY, 
+      api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+    console.log("Cloudinary ENV:", process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
+
     const result = await cloudinary.uploader.upload(file,{
         resource_type:'auto'
     });
